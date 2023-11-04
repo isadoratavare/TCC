0eecfe79efd226e27a9db61564a643936d1c98fd
Isadora Tavares <isadoratavare85@gmail.com>
2023-11-03 22:57:58 -0300

    Init Maps

---
+++ expo-app/src/components/Location.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import { Platform, Text, View, StyleSheet } from "react-native";
+import Device from "expo-device";
+import * as Location from "expo-location";
+import { Marker } from "react-native-maps";
+
+export default function UserLocation() {
+  const [location, setLocation] = useState<Location.LocationObject>();
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS === "android" && !Device.isDevice) {
+        setErrorMsg("Error, try in your phone!");
+        return;
+      }
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+
+  return (
+    <Marker
+      coordinate={location?.coords}
+    ></Marker>
+  );
+}

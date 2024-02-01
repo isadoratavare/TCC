faadbc6b62f9f19b32abbaf0e0097821151a1810
Isadora <isadoratavare85@gmail.com>
2024-01-31 21:12:45 -0300

    react native clean project

--- reactnativeApp/ios/reactnativeAppTests/reactnativeAppTests.m
+++ reactnativeApp/ios/reactnativeAppTests/reactnativeAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reactnativeAppTests : XCTestCase
+@interface reactNativeAppTests : XCTestCase
 
 @end
 
-@implementation reactnativeAppTests
+@implementation reactNativeAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
---
+++ reactnativeApp/src/components/ImageGallery/index.tsx
@@ -0,0 +1,101 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable react/no-unstable-nested-components */
+import React, { useEffect, useState } from 'react';
+import { FlatList, View, Image, Modal } from 'react-native';
+import GalleryButton from '../GalleryButton';
+import {
+    ImageGalleryContextProps,
+    useImageGallery,
+} from '../../hooks/useImageGallery';
+
+const ImageGallery: React.FC<{ placeId: string }> = ({ placeId }) => {
+    const [cameraOrGalleryModalOpen, setCameraOrGalleryModalOpen] =
+        useState<boolean>(false);
+    const [photosData, setPhotoData] = useState<{ id: string; uris: string[]; }>()
+    const { addImageByGallery, photos, addImageByCamera } =
+        useImageGallery() as ImageGalleryContextProps;
+
+    useEffect(() => {
+        setPhotoData(photos.filter((photo) => photo.id === placeId)[0]);
+    }, [photos, placeId]);
+
+    return (
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+            <FlatList
+                data={photosData?.uris}
+                ListHeaderComponent={() => (
+                    <GalleryButton
+                        icon="add-photo-alternate"
+                        name="Adicionar lembrança"
+                        onPress={() => {
+                            setCameraOrGalleryModalOpen(true);
+                        }}
+                    />
+                )}
+                renderItem={({ item }: any) => {
+                    return (
+                        <View>
+                            {item && (
+                                <Image
+                                    source={{ uri: item }}
+                                    style={{
+                                        width: 100,
+                                        height: 100,
+                                        borderRadius: 10,
+                                        marginHorizontal: 5,
+                                    }}
+                                />
+                            )}
+                        </View>
+                    );
+                }}
+                horizontal
+            />
+
+            {cameraOrGalleryModalOpen && (
+                <Modal transparent={true} visible={cameraOrGalleryModalOpen}>
+                    <View
+                        style={{
+                            flex: 1,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            backgroundColor: '#3A3A3A4D',
+                        }}
+                    >
+                        <View
+                            style={{
+                                backgroundColor: 'white',
+                                width: 250,
+                                height: 130,
+                                borderRadius: 15,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                flexDirection: 'row',
+                            }}
+                        >
+                            <GalleryButton
+                                icon="insert-photo"
+                                name="Galeria"
+                                onPress={async () => {
+                                    await addImageByGallery(placeId);
+                                    setCameraOrGalleryModalOpen(false);
+                                }}
+                            />
+                            <GalleryButton
+                                icon="camera-alt"
+                                name="Câmera"
+                                onPress={async () => {
+                                    await addImageByCamera(placeId);
+                                    setCameraOrGalleryModalOpen(false);
+                                }}
+                            />
+                        </View>
+                    </View>
+                </Modal>
+            )}
+        </View>
+    );
+};
+
+export default ImageGallery;

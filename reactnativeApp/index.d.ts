11f7b4955b024b30458dd6af9b8873676f154ac4
Isadora Tavares <isadoratavare85@gmail.com>
2024-11-27 17:31:23 -0300

    extract data time script

--- reactnativeApp/index.d.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable prettier/prettier */
-declare module '@env' {
-  export const NODE_ENV: 'development' | 'production' | 'test';
-  export const GOOGLE_MAPS_KEY: string;
-}
--- reactnativeApp/src/utils/api.ts
+++
@@ -1,2 +0,0 @@
-/* eslint-disable prettier/prettier */
-export const API_URL = 'https://maps.googleapis.com/maps/api/place';

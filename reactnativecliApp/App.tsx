f1498cb7622a16e95726f37129edffb96a7fd118
Isadora Tavares <isadoratavare85@gmail.com>
2025-01-07 22:35:35 -0300

    Revert "update react native app version"
    This reverts commit 8c747c4b75a2485c448ff427eccaf30410968563.

--- reactnativecliApp/App.tsx
+++ reactnativecliApp/App.tsx
@@ -1,7 +1,52 @@
-import React from 'react';
+/* eslint-disable prettier/prettier */
+import React, {  useState } from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+import { LocationProvider } from './src/hooks/useLocation';
+import Map from './src/components/Map';
+import { Pin } from './src/@types/map';
+import { ImageGalleryProvider } from './src/hooks/useImageGallery';
+import ModalBottom from './src/components/ModalBottom';
+import ImageGallery from './src/components/ImageGallery';
+import { MetricsProvider } from './src/hooks/useMetrics';
+
 
 function App(): React.JSX.Element {
-  return <></>;
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [modalMarker, setModalMarker] = useState<Pin | null>();
+
+  return (
+    <LocationProvider>
+      <ImageGalleryProvider>
+        <MetricsProvider>
+          <View style={[StyleSheet.absoluteFillObject, styles.container]}>
+            <Map onPressMarker={(marker: Pin) => {
+              if (!isModalOpen) {
+                setIsModalOpen(true);
+              }
+              setModalMarker(marker);
+            }} />
+          </View>
+          {isModalOpen && (
+            <ModalBottom isOpen={isModalOpen} setOpen={setIsModalOpen}>
+              <ImageGallery placeId={modalMarker?.place_id || ""} />
+            </ModalBottom>
+          )}
+        </MetricsProvider>
+      </ImageGalleryProvider>
+    </LocationProvider>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  map: {
+    flex: 1,
+    width: '100%',
+    height: ' 100%',
+  },
+});
 
 export default App;
